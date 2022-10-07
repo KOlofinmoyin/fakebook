2970e8016e0b3eb4f691dc23ac6a6ed1caa1419a
Kehinde Olofinmoyin <kehindeolofinmoyin@hotmail.com>
2022-10-07 18:58:02 +0100

    Config: Initial setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 3009;
+
+app.set("view engine", "ejs");
+// REST API: Re-presentational State Transfer
+// GET POST PUT DELETE UPDATE
+// www.bbc.com / sport;
+// www.bbc.com / news;
+// www.google.com/books
+// www.fakebook.com = localhost
+// app.get("localhost/homepage");
+
+app.get("/homepage", (request, response) => {
+  response.render("index", { data: { username: "Yassir", loggedIn: true } });
+});
+
+app.listen(PORT, () => {
+  console.log(`Fakebook app. listening on port: ${PORT}.`);
+});
